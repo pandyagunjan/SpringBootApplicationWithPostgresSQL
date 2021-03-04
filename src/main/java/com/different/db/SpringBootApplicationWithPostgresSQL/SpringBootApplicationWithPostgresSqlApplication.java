5cc1b1aea95b9d1d035fc84957995c6aef8677dc
Gunjan Pandya <gunjanpandya@santoshs-air.lan>
2021-03-04 08:59:39 -0500

    message

---
+++ src/main/java/com/different/db/SpringBootApplicationWithPostgresSQL/SpringBootApplicationWithPostgresSqlApplication.java
@@ -0,0 +1,13 @@
+package com.different.db.SpringBootApplicationWithPostgresSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationWithPostgresSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationWithPostgresSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/different/db/SpringBootApplicationWithPostgresSQL/SpringBootApplicationWithPostgresSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.different.db.SpringBootApplicationWithPostgresSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApplicationWithPostgresSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
