738011cc20c3791294eca7714cf5d4803393fbf2
Gunjan Pandya <gunjanpandya@santoshs-air.lan>
2021-03-10 09:09:31 -0500

    added AWS S3 file upload and deletion

--- src/main/java/com/different/db/demo/carRunner/controller/BucketController.java
+++ src/main/java/com/different/db/demo/carRunner/controller/BucketController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
+//https://medium.com/oril/uploading-files-to-aws-s3-bucket-using-spring-boot-483fcb6f8646
 @RestController
 @RequestMapping("/storage/")
 public class BucketController {
